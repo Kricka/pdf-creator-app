f8b23705dc5fdca95c4eece3d2582d37b7926f81
Milan <kricka.milan@gmail.com>
2022-12-27 13:15:49 +0100

    Added lines inside pages

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ for index,row in df.iterrows():
         pdf.set_font(family="Times",style='B',size=12)
         pdf.set_text_color(0,54,56)
         pdf.cell(w=0,h=12,txt=row['Topic'],align='L',ln=1)
-        pdf.line(10,20,200,20)
+        for x in range(20,290,10):
+            pdf.line(10, x, 200, x)
+
 
         # Set the footer
         pdf.ln(260)
@@ -22,16 +24,20 @@ for index,row in df.iterrows():
         pdf.cell(w=0,h=10,txt=f"{row['Topic']}",align="R")
 
 
+
         for x in range(row['Pages']-1):
             pdf.add_page()
             pdf.set_text_color(0,54,56)
             pdf.cell(w=0, h=12, txt="", align='L', ln=1)
+            for x in range(20, 290, 10):
+                pdf.line(10, x, 200, x)
+
             #Set the footer
             pdf.ln(260)
             pdf.set_font(family='Times', style='I', size=10)
             pdf.set_text_color(180,180,180)
             pdf.cell(w=0, h=10, txt=f"{row['Topic']}", align="R")
 
-pdf.output('first.pdf')
+pdf.output('created_pdf.pdf')
 
 
